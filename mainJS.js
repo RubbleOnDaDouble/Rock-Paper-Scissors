6935ae00397a05f7de0ba3f3c0178f276e21301c
RubbleOnDaDouble <172216079+RubbleOnDaDouble@users.noreply.github.com>
2025-08-29 23:11:42 +1000

    Create algorithm for random rock-paper-scissor
    Create the algorithm for transforming the random
    number into a rock-paper-scissor string.
    if the randomized number is less than 0.33 choice
    is rock. if randomized number is between 0.34 and
    0.66 choice is paper. if between 1 and 0.67 choice
    is scissors.

--- mainJS.js
+++ mainJS.js
@@ -10,6 +10,22 @@ function compRandomChoice() {
 
     //generate a random number then round off to 2 decimal places
     randomNum = Math.random().toFixed(2);
+
+    //if else statement to evaluate the random number
+    //if the randomized number is less than 0.33 store Rock to the compChoice variable
+    if (randomNum < 0.33) {
+        compChoice = "Rock";
+    //else if the randomized number is less or equal to 0.66 AND greater than or equal to 0.34 store Paper
+    } else if (randomNum <= 0.66 && randomNum >= 0.34) {
+        compChoice = "Paper";
+    //else if the randomized number is less or equal to 1 AND greater than or equal to 0.67 store Scissors
+    } else if (randomNum <= 1 && randomNum >= 0.67) {
+        compChoice = "Scissors";
+    //else if the computer algorithm failed the computer went BONKERZZ
+    } else {
+        compChoice = "The computer went Meeeehhh!"
+    }
+    return compChoice;
 }
 
-compRandomChoice();
+console.log(compRandomChoice());
