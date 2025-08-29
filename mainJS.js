3c6e074e513b6f6324fad0c322855ed3c99ce75a
RubbleOnDaDouble <172216079+RubbleOnDaDouble@users.noreply.github.com>
2025-08-29 22:59:54 +1000

    Create the function for random comp choice
    Create the initial function for generating the
    computer choice of rock-paper-scissor.
    Create the algorithm for randomizing numbers to
    be used as an evaluation method for generating
    outputs.

---
+++ mainJS.js
@@ -0,0 +1,15 @@
+
+//function to generate the computer choice in console ONLY!
+function compRandomChoice() {
+    
+    //variable for storing random number
+    let randomNum = null;
+
+    //variable for storing the generated output
+    let compChoice = null;
+
+    //generate a random number then round off to 2 decimal places
+    randomNum = Math.random().toFixed(2);
+}
+
+compRandomChoice();
