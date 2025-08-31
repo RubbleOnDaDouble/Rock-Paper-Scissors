b10041c14e5b1005a7ec55199b66e7e4b423dfab
RubbleOnDaDouble <172216079+RubbleOnDaDouble@users.noreply.github.com>
2025-08-31 19:47:26 +1000

    Create function to play 1 round
    Create a function with 2 parameters for input.
    Parameters are converted to lower case to avoid
    case insensitive input. Add scoring using switch
    statements to evaluate parameters. Return result
    as a string. Also added a global variable for
    scoring.

--- mainJS.js
+++ mainJS.js
@@ -1,3 +1,7 @@
+//Global variable for storing computer and player score
+let compScore = 0;
+let playerScore = 0;
+
 
 //function to generate the computer choice in console ONLY!
 function compRandomChoice() {
@@ -14,13 +18,13 @@ function compRandomChoice() {
     //if else statement to evaluate the random number
     //if the randomized number is less than 0.33 store Rock to the compChoice variable
     if (randomNum < 0.33) {
-        compChoice = "Rock";
+        compChoice = "rock";
     //else if the randomized number is less or equal to 0.66 AND greater than or equal to 0.34 store Paper
     } else if (randomNum <= 0.66 && randomNum >= 0.34) {
-        compChoice = "Paper";
+        compChoice = "paper";
     //else if the randomized number is less or equal to 1 AND greater than or equal to 0.67 store Scissors
     } else if (randomNum <= 1 && randomNum >= 0.67) {
-        compChoice = "Scissors";
+        compChoice = "scissors";
     //else if the computer algorithm failed the computer went BONKERZZ
     } else {
         compChoice = "The computer went Meeeehhh!"
@@ -30,9 +34,76 @@ function compRandomChoice() {
 
 //function to get human choice
 function getHumanChoice() {
-    let humanChoice = prompt("Pick your choice! (Rock-Paper-Scissors");
-    return humanChoice;
+    let input = prompt("Pick your choice! (Rock-Paper-Scissors");
+    return input;
+}
+
+//function to play 1 round
+function playRound(hChoice,cChoice) {
+
+    //convert parameters to lowercase to be case insensitive
+    hChoice = hChoice.toLowerCase();
+    cChoice = cChoice.toLowerCase();
+
+    //variable to store results
+    let result = null;
+    
+    //switch statement to to evaluate human choice
+    switch (hChoice) {
+        case "rock":
+            //switch statement to compare comp choice to human choice
+            switch (cChoice) {
+                case "rock":
+                    result = "It's a tie!";
+                    break;
+                case "paper":
+                    result = "You Lose! paper beats rock!";
+                    compScore++;
+                    break;
+                case "scissors":
+                    result = "You Win! rock beats scissors!";
+                    playerScore++;
+                    break;
+            }
+            break;
+        case "paper":
+            //switch statement to compare comp choice to human choice
+            switch (cChoice) {
+                case "rock":
+                    result = "You Win! paper beats rock";
+                    playerScore++;
+                    break;
+                case "paper":
+                    result = "It's a tie!";
+                    break;
+                case "scissors":
+                    result = "You Lose! scissors beats paper!";
+                    compScore++;
+                    break;
+            }
+        break;
+        case "scissors":
+            //switch statement to compare comp choice to human choice
+            switch (cChoice) {
+                case "rock":
+                    result = "You Lose! rock beats scissors";
+                    compScore++;
+                    break;
+                case "paper":
+                    result = "You Win! scissors beats rock";
+                    playerScore++;
+                    break;
+                case "scissors":
+                    result = "It's a tie!"
+                    break;
+            }
+        break;
+    }
+
+    return console.log(result);
 }
 
 //console.log(compRandomChoice());
-console.log(getHumanChoice())
+//console.log(getHumanChoice())
+//playRound("PapEr","RocK");
+//console.log(compScore + " " + playerScore);
