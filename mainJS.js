f7736139eed20f8f938f1c84790871302bbb5663
RubbleOnDaDouble <172216079+RubbleOnDaDouble@users.noreply.github.com>
2025-09-04 17:57:52 +1000

    Create funtion to play game
    Create function to play 5 rounds. Moved the play
    round funtion within the playGame function.
    Move global variable for scores within the
    playGame function.

--- mainJS.js
+++ mainJS.js
@@ -1,8 +1,3 @@
-//Global variable for storing computer and player score
-let compScore = 0;
-let playerScore = 0;
-
-
 //function to generate the computer choice in console ONLY!
 function compRandomChoice() {
     
@@ -38,75 +33,106 @@ function getHumanChoice() {
     return input;
 }
 
-//function to play 1 round
-function playRound(hChoice,cChoice) {
+//function to play a game with 5 rounds
+function playGame(humChoice,comChoice) {
 
-    //convert parameters to lowercase to be case insensitive
-    hChoice = hChoice.toLowerCase();
-    cChoice = cChoice.toLowerCase();
+    //variable for storing computer and player score
+    let compScore = 0;
+    let playerScore = 0;
 
-    //variable to store results
-    let result = null;
-    
-    //switch statement to to evaluate human choice
-    switch (hChoice) {
-        case "rock":
-            //switch statement to compare comp choice to human choice
-            switch (cChoice) {
-                case "rock":
-                    result = "It's a tie!";
-                    break;
-                case "paper":
-                    result = "You Lose! paper beats rock!";
-                    compScore++;
-                    break;
-                case "scissors":
-                    result = "You Win! rock beats scissors!";
-                    playerScore++;
-                    break;
-            }
-            break;
-        case "paper":
-            //switch statement to compare comp choice to human choice
-            switch (cChoice) {
-                case "rock":
-                    result = "You Win! paper beats rock";
-                    playerScore++;
-                    break;
-                case "paper":
-                    result = "It's a tie!";
-                    break;
-                case "scissors":
-                    result = "You Lose! scissors beats paper!";
-                    compScore++;
-                    break;
-            }
-            break;
-        case "scissors":
-            //switch statement to compare comp choice to human choice
-            switch (cChoice) {
-                case "rock":
-                    result = "You Lose! rock beats scissors";
-                    compScore++;
-                    break;
-                case "paper":
-                    result = "You Win! scissors beats rock";
-                    playerScore++;
-                    break;
-                case "scissors":
-                    result = "It's a tie!"
-                    break;
-            }
-            break;
-        default:
-            result = "Enter a valid choice between rock-paper-scissors";
-            break;
+    //variable for set number of rounds to be played
+    let rounds = 0;
+
+    //function to play 1 round
+    function playRound(hChoice,cChoice) {
+
+        //convert parameters to lowercase to be case insensitive
+        hChoice = hChoice.toLowerCase();
+        cChoice = cChoice.toLowerCase();
+
+        //variable to store results
+        let result = null;
+        
+        //switch statement to to evaluate human choice
+        switch (hChoice) {
+            case "rock":
+                //switch statement to compare comp choice to human choice
+                switch (cChoice) {
+                    case "rock":
+                        result = "It's a tie!";
+                        break;
+                    case "paper":
+                        result = "You Lose! paper beats rock!";
+                        compScore++;
+                        break;
+                    case "scissors":
+                        result = "You Win! rock beats scissors!";
+                        playerScore++;
+                        break;
+                }
+                break;
+            case "paper":
+                //switch statement to compare comp choice to human choice
+                switch (cChoice) {
+                    case "rock":
+                        result = "You Win! paper beats rock";
+                        playerScore++;
+                        break;
+                    case "paper":
+                        result = "It's a tie!";
+                        break;
+                    case "scissors":
+                        result = "You Lose! scissors beats paper!";
+                        compScore++;
+                        break;
+                }
+                break;
+            case "scissors":
+                //switch statement to compare comp choice to human choice
+                switch (cChoice) {
+                    case "rock":
+                        result = "You Lose! rock beats scissors";
+                        compScore++;
+                        break;
+                    case "paper":
+                        result = "You Win! scissors beats rock";
+                        playerScore++;
+                        break;
+                    case "scissors":
+                        result = "It's a tie!"
+                        break;
+                }
+                break;
+            default:
+                result = "Enter a valid choice between rock-paper-scissors";
+                break;
+        }
+
+        return (result);
     }
 
-    return console.log(result);
+    //create a loop to run the game for 5 rounds
+    do {
+        //call function to play 1 round
+        playRound(humChoice,comChoice);
+
+        //add 1 round after game played
+        rounds++;
+
+        //declare the winner after 5 rounds
+        if (rounds = 5) {
+            //evaluate the scores between computer and player
+            if (playerScore < compScore) {
+                console.log("Player wins the game! player score:" + playerScore + " computer score:" + compScore );
+            } else {
+                console.log("Computer wins the game! player score:" + playerScore + " computer score:" + compScore )
+            }
+        }
+    } while (rounds >= 5);
 }
 
 //console.log(compRandomChoice());
 //console.log(getHumanChoice())
 //playRound("PapEr","RocK");
 //console.log(compScore + " " + playerScore);
+playGame(getHumanChoice(),compRandomChoice());
